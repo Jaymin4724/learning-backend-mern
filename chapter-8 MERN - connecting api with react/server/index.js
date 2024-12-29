8f4f07f4a8415523aeeaa5f1142c6208a178942c
jaymin dave <jaymin4724@gmail.com>
2024-12-29 14:56:53 +0530

    Chapter 8 Completed

--- chapter-8 MERN - connecting api with react/server/index.js
+++ chapter-8 MERN - connecting api with react/server/index.js
@@ -4,24 +4,47 @@ import mongoose from "mongoose";
 import cors from "cors";
 import productRouter from "./routes/product-routes.js";
 import userRouter from "./routes/user-routes.js";
-
+import path, { dirname } from "path";
 const server = express();
 
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+// Validate required environment variables
+if (!process.env.DB_PASSWORD || !process.env.PUBLIC_DIR || !process.env.PORT) {
+  console.error("Missing required environment variables!");
+  process.exit(1); // Exit if any required variable is missing
+}
+
 // db connection
-// mongoose.connect("mongodb://localhost:27017/ecommerce");
-main().catch((err) => console.log(err));
+main().catch((err) => {
+  console.error("DB Connection failed: ", err);
+});
 async function main() {
-  await mongoose.connect("mongodb://localhost:27017/ecommerce");
-  console.log("mongooseeeeebumps --> DB Connected !!");
-  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+  try {
+    await mongoose.connect(`mongodb://localhost:27017/ecommerce`);
+    console.log("mongooseeeeebumps --> DB Connected !!");
+  } catch (error) {
+    console.error("Database connection error: ", error);
+  }
 }
 
 server.use(express.json()); // BodyParser
-console.log("env -->", process.env.DB_PASSWORD);
+
+// Avoid logging sensitive data like DB_PASSWORD
+// console.log("env -->", process.env.DB_PASSWORD);
+
 // Middleware for routers
 server.use(cors());
+server.use(express.static(path.resolve(__dirname, process.env.PUBLIC_DIR)));
 server.use("/products", productRouter);
 server.use("/users", userRouter);
+
+// for wildcard routes (react routes)
+server.use("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, process.env.PUBLIC_DIR, "index.html"));
+});
 
 server.listen(process.env.PORT, () => {
   console.log(`Server Started at http://localhost:${process.env.PORT}/`);
